7d288b26dcc2a4ffed99af2b2726039d020f60af
Robert Stine <rcorystine@gmail.com>
2020-12-17 18:40:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,14 @@
 def square_array(array)
+<<<<<<< HEAD
   squared = []
   array.each do |num|
     squared << num**2
   end
   squared
+=======
+  array.each do |square|
+    square**2
+  end
+  square_array(array)
+>>>>>>> 2ce468de7a09a75697857eaa1039ec26b8b486f8
 end
